0ee3868aa6583ca5e50c48fa01186601fd05687e
aabbccdd <choishuyi@gmail.com>
2022-04-06 03:26:47 +1000

    change

---
+++ static/js/header.js
@@ -0,0 +1,10 @@
+function show(){
+    var nav = document.getElementById("nav");
+    nav.style.display="block";
+}
+
+function hide(){
+    var nav = document.getElementById("nav");
+    nav.style.display="none";
+
+}
