48fb48ceab28efe4ac8fe6fbb99c0ea237adc0a9
aabbccdd <choishuyi@gmail.com>
2022-04-07 01:02:58 +1000

    update

--- static/js/slide.js
+++ static/js/slide.js
@@ -1,7 +1,24 @@
-function next(){
+class slide{
+    constructor(){
+        this.left_btn = document.querySelector(".left");
+        this.rigth_btn = document.querySelector(".right");
+        this.init();
+    }
+
+    init(){
+        this.left_btn.onclick = this.left;
+        // this.rigth_btn.onclick = this.right;
+    }
+
+    left(){
+        console.log("left");
+
+    }
+
+    right(){
+        console.log("right");
+    }
 
 }
 
-function last(){
-    
-}
+new slide();
